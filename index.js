c01bdacaa980bd7a78d6920c7eb6d25e1558c06a
Juke-99 <yanpahika@gmail.com>
2019-10-13 22:09:15 +0900

    add compact and babel

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const lodash = require('lodash')
+// const chunk = require('./chunk')
+// const compact = require('./compact')
 
 var a = ['a', 'b', 'c']
 var b = [3, 4, 45, 6, 3, 2, 6, 7, 3, 4, 45, 6, 3, 2, 6, 7]
+var c = [null, NaN, undefined, '', 2, 'ioyge', false]
 
-console.log(lodash.chunk(a, 2))
-console.log(lodash.chunk(a))
+console.log("lodash chunk 1: " + lodash.chunk(a, 2))
+console.log("lodash chunk 2: " + lodash.chunk(a))
 
-console.log(myChunk(b, 3))
+console.log("origin chunk 1: " + myChunk(b, 3))
+console.log("origin compat 1: " + myCompact(c))
+
+// console.log("origin chunk 1: " + chunk(b, 3))
+// console.log("origin compat 1: " + compact(c))
+
+/**
+ * Babel isn't runing... 
+ * ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+ */
 
 function myChunk(array, size) {
   let index = 0, resultLength = 0, length = array.length, result = Array(Math.ceil(length / size))
@@ -28,6 +40,19 @@ function arraySlice(array, start, end) {
 
   while(++index < length) {
     result[index] = array[index + start]
+  }
+
+  return result
+}
+
+function myCompact(array) {
+  let result = [], length = array.length, index = -1, resultIndex = 0
+
+  while(++index < length) {
+    let value = array[index]
+    if(value) {
+      result[resultIndex++] = array[index]
+    }
   }
 
   return result
