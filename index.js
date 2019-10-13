7105e496b0f298de66cbfeed3d19708e656d544e
Juke-99 <yanpahika@gmail.com>
2019-10-14 00:50:33 +0900

    add comment fix console

--- index.js
+++ index.js
@@ -6,11 +6,25 @@ var a = ['a', 'b', 'c']
 var b = [3, 4, 45, 6, 3, 2, 6, 7, 3, 4, 45, 6, 3, 2, 6, 7]
 var c = [null, NaN, undefined, '', 2, 'ioyge', false]
 
-console.log("lodash chunk 1: " + lodash.chunk(a, 2))
-console.log("lodash chunk 2: " + lodash.chunk(a))
+console.log("lodash chunk 1:")
+console.log(lodash.chunk(a, 2))
+console.log("")
 
-console.log("origin chunk 1: " + myChunk(b, 3))
-console.log("origin compat 1: " + myCompact(c))
+console.log("lodash chunk 2:")
+console.log(lodash.chunk(a))
+console.log("")
+
+console.log("origin chunk 1: ")
+console.log(myChunk(b, 3))
+console.log("")
+
+console.log("lodash compact:")
+console.log(lodash.compact(c))
+console.log("")
+
+console.log("origin compat 1: ")
+console.log(myCompact(c))
+console.log("")
 
 // console.log("origin chunk 1: " + chunk(b, 3))
 // console.log("origin compat 1: " + compact(c))
@@ -20,6 +34,13 @@ console.log("origin compat 1: " + myCompact(c))
  * ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
  */
 
+/**
+ * lodash chunk method
+ * @param {Array} array 
+ * @param {Number} size 
+ * 
+ * @returns Array
+ */
 function myChunk(array, size) {
   let index = 0, resultLength = 0, length = array.length, result = Array(Math.ceil(length / size))
 
@@ -45,6 +66,10 @@ function arraySlice(array, start, end) {
   return result
 }
 
+/**
+ * lodash compact
+ * @param {*} array 
+ */
 function myCompact(array) {
   let result = [], length = array.length, index = -1, resultIndex = 0
 
